e8b98fda214f6d210129cbeca7c707094506c683
Francesco Boffa <fra.boffa@gmail.com>
2021-11-21 19:46:19 +0000

    Logger module

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,22 +1,70 @@
-use std::io::{self, Write};
+use std::io::Write;
+use std::cell::RefCell;
+
+#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub enum Level {
+    Debug, Info, Warn, Error, Off
+}
 
 #[macro_export]
-macro_rules! log {
-    ($($arg:tt)*) => ({
-        let mut out = io::stdout();
+macro_rules! dbg {
+    ($logger:expr,$($arg:tt)*) => ({
         let s = std::format!("{}", std::format_args!($($arg)*));
-        out.write_all(s.as_bytes());
+        $logger.write(Level::Debug, s);
     })
 }
 
-struct Logger<'a> {
-    name: &'a str
+#[macro_export]
+macro_rules! warn {
+    ($logger:expr,$($arg:tt)*) => ({
+        let s = std::format!("{}", std::format_args!($($arg)*));
+        $logger.write(Level::Warn, s);
+    })
 }
 
-impl Logger<'_> {
-    fn new(name: &str) -> Logger {
+#[macro_export]
+macro_rules! info {
+    ($logger:expr,$($arg:tt)*) => ({
+        let s = std::format!("{}", std::format_args!($($arg)*));
+        $logger.write(Level::Info, s);
+    })
+}
+
+#[macro_export]
+macro_rules! err {
+    ($logger:expr,$($arg:tt)*) => ({
+        let s = std::format!("{}", std::format_args!($($arg)*));
+        $logger.write(Level::Error, s);
+    })
+}
+
+pub struct Logger {
+    name: String,
+    level: Level,
+    out: RefCell<Box<dyn Write>>,
+}
+
+impl Logger {
+    pub fn new(name: &str, level: Level) -> Logger {
         Logger {
-            name
+            name: String::from(name),
+            level,
+            out: RefCell::new(Box::new(std::io::stdout()))
+        }
+    }
+
+    pub fn new_with_out(name: &str, level: Level, out: Box<dyn Write>) -> Logger {
+        Logger {
+            name: String::from(name),
+            level,
+            out: RefCell::new(out),
+        }
+    }
+
+    pub fn write(&self, level: Level, line: String) {
+        if level >= self.level {
+            let s = std::format!("[{}] {}\n", self.name, line);
+            self.out.borrow_mut().write_all(s.as_bytes()).expect("Could not write a log line");
         }
     }
 }
@@ -25,8 +73,27 @@ impl Logger<'_> {
 mod test {
     use super::*;
 
+    struct Tester {
+        logger: Logger
+    }
+
+    impl Tester {
+        fn new(logger: Logger) -> Tester {
+            Tester { logger }
+        }
+
+        fn run(&self) {
+            dbg!(self.logger, "A debugging message");
+            info!(self.logger, "An info message");
+            warn!(self.logger, "A warning");
+            err!(self.logger, "An error");
+        }
+    }
+
+    /// Run with --nocapture to acknowledge the results
     #[test]
-    fn it_works() {
-        log!("Hi {:08x}", 123);
+    fn it_prints_to_stdout() {
+        let t = Tester::new(Logger::new("TST", Level::Warn));
+        t.run();
     }
 }
